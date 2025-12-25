92ff88478ff16d8f54ec872a89b1b050d17506c1
Gohanpro9 <166078096+Gohanpro9@users.noreply.github.com>
2025-12-24 18:09:05 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "v0.5"
+
+self.addEventListener("activate", e => {
   e.waitUntil(
-    caches.open("app").then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./App.js"
-      ])
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
     )
   )
 })
